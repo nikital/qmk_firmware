3758b5bdc76cf8fabe9c12ffb09fd8b34955ca1f
Nikita Leshenko <nikita@leshenko.net>
2022-02-17 09:25:25 +0200

    New layers

--- keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
+++ keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
@@ -6,6 +6,8 @@
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
     _BASE,
+    _RL,
+    _LL,
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -15,7 +17,6 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base */
     [_BASE] = LAYOUT(
 KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,           KC_F6, KC_F7, KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,   KC_PSCR, KC_SCRL, KC_PAUS,
 
@@ -23,6 +24,24 @@ KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,           KC_F6, KC_F7, KC_F8,  KC_
 KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T,                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL, KC_PGDN,
 KC_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G,                KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,  KC_NO ,           KC_HOME, KC_END,
 KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B,                KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,               KC_UP,
-KC_LCTL, KC_LGUI, KC_LALT,      KC_NO,  KC_SPC,        KC_ENT, KC_BSPC,   KC_RALT, KC_NO, KC_RCTL, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
+KC_LCTL, KC_LGUI, KC_LALT,      MO(_LL),  KC_SPC,        MO(_RL), KC_BSPC,   KC_RALT, KC_NO, KC_RCTL, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [_RL] = LAYOUT(
+KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+
+ KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, KC_NO,   KC_NO,         KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_ENT, KC_NO, KC_NO, KC_NO,   KC_NO,                KC_NO,
+KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO,          KC_TRNS, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
+    ),
+    [_LL] = LAYOUT(
+KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO,
+
+ KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO,   KC_NO,         KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_0, KC_NO, KC_DOT, KC_NO,   KC_NO,                KC_NO,
+KC_NO, KC_NO, KC_NO,      KC_TRNS, KC_NO,          KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
     ),
 };
