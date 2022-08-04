a933ef483847c34c0eca9889e318dcd4576b7bfe
Nikita Leshenko <nikita@leshenko.net>
2022-08-04 11:49:16 -0400

    Missing numlock keys

--- keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
+++ keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
@@ -38,9 +38,9 @@ KC_TRNS, KC_TRNS, KC_TRNS,        KC_NO, KC_NO,   KC_TRNS, KC_NO,   KC_TRNS, KC_
     [_LL] = LAYOUT(
 KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NUMLOCK, KC_NO,
 
- KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_7, KC_KP_8, KC_9, KC_NO, KC_KP_MINUS, KC_NO, KC_NO,    KC_NO, KC_NO,
-KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_4, KC_KP_5, KC_6, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
-KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_1, KC_KP_2, KC_3, KC_NO, KC_NO,   KC_NO,         KC_NO, KC_NO,
+ KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_7, KC_KP_8, KC_KP_9, KC_NO, KC_KP_MINUS, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_4, KC_KP_5, KC_KP_6, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
+KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_1, KC_KP_2, KC_KP_3, KC_NO, KC_NO,   KC_NO,         KC_NO, KC_NO,
 KC_NO,   KC_NO, KC_NO, LCTL(KC_INS), LSFT(KC_INS), KC_NO,   KC_NO, KC_KP_0, KC_NO, KC_KP_DOT, KC_NO,   KC_NO,                KC_NO,
 KC_NO, KC_NO, KC_NO,      KC_TRNS, KC_NO,                 KC_NO, KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
     ),
