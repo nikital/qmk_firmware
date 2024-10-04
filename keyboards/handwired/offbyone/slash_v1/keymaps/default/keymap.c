f97a3c5faf6802cb15c598e8e4c8550e6646d660
Nikita Leshenko <nikita@leshenko.net>
2024-10-04 23:54:34 +0300

    Revert "Let's try swapping CTRL"
    This reverts commit c2408554506eeba01c7f7e13140daed44d44db4a.

--- keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
+++ keyboards/handwired/offbyone/slash_v1/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ KC_ESC,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,           KC_F6, KC_F7, KC_F8,  KC_
 KC_TAB,   KC_Q, KC_W, KC_E, KC_R, KC_T,                KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL, KC_PGDN,
 KC_CAPS,  KC_A, KC_S, KC_D, KC_F, KC_G,                KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,  KC_ENT ,          KC_HOME, KC_END,
 KC_LSFT,  KC_Z, KC_X, KC_C, KC_V, KC_B,                KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,               KC_UP,
-MO(_LL), KC_LGUI, KC_LALT,      KC_LCTL,  KC_SPC,        MO(_RL), KC_BSPC,   KC_RALT, KC_DEL, KC_RCTL, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
+KC_LCTL, KC_LGUI, KC_LALT,      MO(_LL),  KC_SPC,        MO(_RL), KC_BSPC,   KC_RALT, KC_DEL, KC_RCTL, KC_APP,  KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_RL] = LAYOUT(
 KC_TRNS,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS,
@@ -33,7 +33,7 @@ KC_TRNS,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO
 KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, LCTL(KC_PGUP), LCTL(KC_PGDN), KC_NO, KC_NO, KC_PGUP, KC_PGDN, KC_TRNS,     KC_NO, KC_NO,
 KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO, KC_NO,   KC_TRNS,       KC_NO, KC_NO,
 KC_TRNS,   KC_NO, KC_NO, LCTL(KC_INS), LSFT(KC_INS), KC_NO,        KC_ESC, KC_ENT, KC_HOME, KC_END, KC_NO,   KC_TRNS,                KC_NO,
-KC_NO, KC_TRNS, KC_TRNS,        KC_TRNS, KC_NO,   KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_NO, KC_NO, KC_NO
+KC_TRNS, KC_TRNS, KC_TRNS,        KC_NO, KC_NO,   KC_TRNS, KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_NO, KC_NO, KC_NO
     ),
     [_LL] = LAYOUT(
 KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NUM_LOCK, KC_NO,
@@ -42,6 +42,6 @@ KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,        KC_NO, KC_NO, KC_NO,  KC_NO, 
 KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_4, KC_KP_5, KC_KP_6, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO,
 KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_1, KC_KP_2, KC_KP_3, KC_NO, KC_NO,   KC_NO,         KC_NO, KC_NO,
 KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                 KC_NO, KC_KP_0, KC_NO, KC_KP_DOT, KC_NO,   KC_NO,                KC_NO,
-KC_TRNS, KC_NO, KC_NO,      KC_NO, KC_NO,                 KC_NO, KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
+KC_NO, KC_NO, KC_NO,      KC_TRNS, KC_NO,                 KC_NO, KC_TRNS,   KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO, KC_NO, KC_NO
     ),
 };
